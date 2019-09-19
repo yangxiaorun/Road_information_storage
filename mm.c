48b8cbe4b239fa8fe82c9e01cb53a44b0a31b34b
YANGXIAORUN <33563685+yangxiaorun@users.noreply.github.com>
2019-09-19 20:34:46 +0800

    VScode C环境搭建

---
+++ mm.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello world! \n");
+    //sysytem("pasuse");
+    getch();
+    return 0;
+}
